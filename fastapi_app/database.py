bc071b08c49f991ddb7c9b927c24417348572079
ahmadbahlool <billoahmad25@gmail.com>
2025-10-12 18:33:00 +0500

    hardcoded the host values

--- fastapi_app/database.py
+++ fastapi_app/database.py
@@ -2,7 +2,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from .config import env_variables
-DATABASE_URL=f"postgresql://{env_variables.dbusername}:{env_variables.dbpassword}@{env_variables.dbhost}:{env_variables.dbport}/{env_variables.dbname}"
+DATABASE_URL=f"postgresql://{env_variables.dbusername}:{env_variables.dbpassword}@metro.proxy.rlwy.net:21465/{env_variables.dbname}"
 
 engine=create_engine(DATABASE_URL)
 SessionLocal=sessionmaker(autoflush=True,bind=engine)
