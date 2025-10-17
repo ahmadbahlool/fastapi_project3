c7136832d0282538e597fa47f14f75eebeb8eec9
ahmadbahlool <billoahmad25@gmail.com>
2025-10-17 15:17:30 +0500

    add dummychanges to the hello.py

--- fastapi_app/hello.py
+++ fastapi_app/hello.py
@@ -24,7 +24,7 @@ app.add_middleware(
 )
 @app.get("/")
 def returnst():
-  return {"message":"successful"}
+  return {"message":"successful is me"}
 router=APIRouter()
 router.include_router(users.router)
 router.include_router(products.router)
