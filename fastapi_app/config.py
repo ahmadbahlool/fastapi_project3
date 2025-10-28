85283a6d0088b9b97f9627a1794e4e129720168a
ahmadbahlool <billoahmad25@gmail.com>
2025-10-29 00:12:09 +0500

    add vercel cd pipeline

--- fastapi_app/config.py
+++ fastapi_app/config.py
@@ -1,11 +1,7 @@
 from pydantic_settings import BaseSettings
 
 class Env(BaseSettings):
-  dbhost:str
-  dbport:str
-  dbname:str
-  dbusername:str
-  dbpassword:str
+ 
   secretkey:str
   access_token_expires_minutes:int
   algorithm:str
